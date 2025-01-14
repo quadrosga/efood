32e0a48b79408938868c7c5974a06900811437eb
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-01-14 16:08:23 -0300

    validação com formik, yup

--- src/components/Delivery/index.tsx
+++ src/components/Delivery/index.tsx
@@ -1,11 +1,19 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { updateDelivery } from '../../store/reducers/delivery';
 import { goToCart, goToPayment } from '../../store/reducers/app';
 import Button from '../Button';
 import { CardContainer, Overlay, Sidebar } from './styles';
 import { open } from '../../store/reducers/cart';
+import { usePurchaseMutation } from '../../services/api';
+import { RootReducer } from '../../store';
 
 const Delivery = () => {
   const dispatch = useDispatch();
+  const [purchase] = usePurchaseMutation();
+
+  const { items } = useSelector((state: RootReducer) => state.cart);
 
   const continueToPayment = () => {
     dispatch(goToPayment());
@@ -16,51 +24,143 @@ const Delivery = () => {
     dispatch(open());
   };
 
+  const form = useFormik({
+    initialValues: {
+      receiver: '',
+      description: '',
+      city: '',
+      zipCode: '',
+      number: '',
+      complement: '',
+    },
+    validationSchema: Yup.object({
+      receiver: Yup.string().required('O nome do recebedor é obrigatório.'),
+      description: Yup.string().required('O endereço é obrigatório.'),
+      city: Yup.string().required('A cidade é obrigatória.'),
+      zipCode: Yup.string()
+        .required('O CEP é obrigatório.')
+        .matches(/^\d{5}-?\d{3}$/, 'CEP inválido. Exemplo: 12345-678'),
+      number: Yup.number()
+        .typeError('O número deve ser numérico.')
+        .required('O número é obrigatório.'),
+      complement: Yup.string(),
+    }),
+    onSubmit: async (values) => {
+      dispatch(
+        updateDelivery({
+          receiver: values.receiver,
+          address: {
+            description: values.description,
+            city: values.city,
+            zipCode: values.zipCode,
+            number: Number(values.number),
+            complement: values.complement,
+          },
+        })
+      );
+      dispatch(goToPayment());
+    },
+  });
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isTouched = fieldName in form.touched;
+    const isInvalid = fieldName in form.errors;
+
+    if (isTouched && isInvalid) return message;
+    return '';
+  };
+
   return (
-    <CardContainer>
-      <Overlay />
-      <Sidebar>
-        <h3>Entrega</h3>
-        <div>
-          <label htmlFor="fullName">Quem irá receber</label>
-          <input id="fullName" type="text" />
-        </div>
-        <div>
-          <label htmlFor="address">Endereço</label>
-          <input id="address" type="text" />
-        </div>
-        <div>
-          <label htmlFor="city">Cidade</label>
-          <input id="city" type="text" />
-        </div>
-        <div>
-          <label htmlFor="cep">CEP</label>
-          <input id="cep" type="text" />
-        </div>
-        <div>
-          <label htmlFor="num">Número</label>
-          <input id="num" type="text" />
-        </div>
-        <div>
-          <label htmlFor="comp">Complemento (opcional)</label>
-          <input id="comp" type="text" className="margin-bottom" />
-        </div>
-        <Button
-          type="button"
-          title="Clique aqui para continuer com a entrega"
-          onClick={continueToPayment}
-        >
-          Continuar com a entrega
-        </Button>
-        <Button
-          type="button"
-          title="Clique aqui para retornar ao carrinho"
-          onClick={ReturnToCart}
-        >
-          Voltar para o carrinho
-        </Button>
-      </Sidebar>
-    </CardContainer>
+    <form onSubmit={form.handleSubmit}>
+      <CardContainer>
+        <Overlay onClick={ReturnToCart} />
+        <Sidebar>
+          <h3>Entrega</h3>
+          <div>
+            <label htmlFor="receiver">Quem irá receber</label>
+            <input
+              id="receiver"
+              type="text"
+              value={form.values.receiver}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('receiver', form.errors.receiver)}</small>
+          </div>
+          <div>
+            <label htmlFor="description">Endereço</label>
+            <input
+              id="description"
+              type="text"
+              value={form.values.description}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {' '}
+              {getErrorMessage('description', form.errors.description)}
+            </small>
+          </div>
+          <div>
+            <label htmlFor="city">Cidade</label>
+            <input
+              id="city"
+              type="text"
+              value={form.values.city}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('city', form.errors.city)}</small>
+          </div>
+          <div>
+            <label htmlFor="zipCode">CEP</label>
+            <input
+              id="zipCode"
+              type="text"
+              value={form.values.zipCode}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('zipCode', form.errors.zipCode)}</small>
+          </div>
+          <div>
+            <label htmlFor="number">Número</label>
+            <input
+              id="number"
+              type="text"
+              value={form.values.number}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('number', form.errors.number)}</small>
+          </div>
+          <div>
+            <label htmlFor="complement">Complemento (opcional)</label>
+            <input
+              id="complement"
+              type="text"
+              value={form.values.complement}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+          </div>
+          <Button
+            type="button"
+            title="Clique aqui para continuar com o pagamento"
+            onClick={form.handleSubmit}
+          >
+            Continuar com o pagamento
+          </Button>
+          <Button
+            type="button"
+            title="Clique aqui para voltar para o carrinho"
+            onClick={ReturnToCart}
+          >
+            Voltar para o carrinho
+          </Button>
+        </Sidebar>
+      </CardContainer>
+    </form>
   );
 };
 
--- src/components/Payment/index.tsx
+++ src/components/Payment/index.tsx
@@ -1,72 +1,231 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { setOrderId, updatePayment } from '../../store/reducers/payment';
+import { updateDelivery } from '../../store/reducers/delivery';
+import { goToConfirmation, goToDelivery } from '../../store/reducers/app';
+import { usePurchaseMutation } from '../../services/api';
 import Button from '../Button';
 import { CardContainer, Overlay, Sidebar } from './styles';
-import { goToConfirmation, goToDelivery } from '../../store/reducers/app';
-import { formataPreco } from '../../Modal';
 import { RootReducer } from '../../store';
+import { formataPreco } from '../../Modal';
+import { useEffect } from 'react';
 
 const Payment = () => {
   const dispatch = useDispatch();
+  const [purchase, { isLoading, isError, isSuccess, data, error }] =
+    usePurchaseMutation();
+
+  useEffect(() => {
+    console.log('Mutation State:', {
+      isLoading,
+      isError,
+      isSuccess,
+      data,
+      error,
+    });
+  }, [isLoading, isError, isSuccess, data, error]);
 
   const { items } = useSelector((state: RootReducer) => state.cart);
+  const { receiver, address } = useSelector(
+    (state: RootReducer) => state.delivery
+  );
 
   const getTotalPrice = () => {
     return items.reduce((acc, item) => acc + item.preco, 0);
   };
 
-  const continueToConfirmation = () => {
-    const orderId = crypto.randomUUID().substring(0, 8);
-    dispatch(goToConfirmation(orderId));
+  const continueToConfirmation = async (values: any) => {
+    const purchaseData = {
+      products: items.map((item) => ({ id: item.id, price: item.preco })),
+      delivery: {
+        receiver: receiver,
+        address: {
+          ...address,
+          number: String(address.number),
+        },
+      },
+      payment: {
+        card: {
+          name: values.cardDisplayName,
+          number: String(values.cardNumber),
+          code: Number(values.cardCode),
+          expires: {
+            month: Number(values.expiresMonth),
+            year: Number(values.expiresYear),
+          },
+        },
+      },
+    };
+
+    try {
+      const response = await purchase(purchaseData).unwrap();
+      console.log('Resposta da purchase mutation:', response);
+
+      if (response && response.orderId) {
+        dispatch(setOrderId(response.orderId));
+        dispatch(goToConfirmation(response.orderId));
+      } else {
+        console.error('Order ID não disponível');
+      }
+    } catch (error) {
+      console.error('Erro ao processar o pagamento', error);
+    }
   };
 
   const returnToDelivery = () => {
     dispatch(goToDelivery());
   };
+
+  const form = useFormik({
+    initialValues: {
+      cardDisplayName: '',
+      cardNumber: '',
+      cardCode: '',
+      cep: '',
+      expiresMonth: '',
+      expiresYear: '',
+    },
+    validationSchema: Yup.object({
+      cardDisplayName: Yup.string().required('O campo é obrigatório.'),
+      cardNumber: Yup.string().required('O campo é obrigatório.'),
+      cardCode: Yup.string().required('O campo é obrigatório.'),
+      cep: Yup.string().required('O campo é obrigatório.'),
+      expiresMonth: Yup.string().required('O campo é obrigatório.'),
+      expiresYear: Yup.string().required('O campo é obrigatório.'),
+    }),
+    onSubmit: async (values) => {
+      dispatch(
+        updatePayment({
+          card: {
+            name: values.cardDisplayName,
+            number: values.cardNumber,
+            code: Number(values.cardCode),
+            expires: {
+              month: Number(values.expiresMonth),
+              year: Number(values.expiresYear),
+            },
+          },
+        })
+      );
+
+      dispatch(
+        updateDelivery({
+          receiver: receiver,
+          address: address,
+        })
+      );
+
+      continueToConfirmation(values);
+    },
+  });
+
+  const getErrorMessage = (fieldName: string, message?: string) => {
+    const isTouched = fieldName in form.touched;
+    const isInvalid = fieldName in form.errors;
+
+    if (isTouched && isInvalid) return message;
+    return '';
+  };
+
   return (
-    <CardContainer>
-      <Overlay />
-      <Sidebar>
-        <h3>Pagamento - Valor a pagar {formataPreco(getTotalPrice())}</h3>
-        <div>
-          <label htmlFor="cardDisplayName">Nome no cartão</label>
-          <input id="cardDisplayName" type="text" />
-        </div>
-        <div>
-          <label htmlFor="cardNumber">Número do cartão</label>
-          <input id="cardNumber" type="text" />
-        </div>
-        <div>
-          <label htmlFor="cardCode">CVV</label>
-          <input id="cardCode" type="text" />
-        </div>
-        <div>
-          <label htmlFor="cep">CEP</label>
-          <input id="cep" type="text" />
-        </div>
-        <div>
-          <label htmlFor="expiresMonth">Mês de vencimento</label>
-          <input id="expiresMonth" type="text" />
-        </div>
-        <div>
-          <label htmlFor="expiresYear">Ano de vencimento</label>
-          <input id="expiresYear" type="text" className="margin-bottom" />
-        </div>
-        <Button
-          type="button"
-          title="Clique aqui para continuer com a entrega"
-          onClick={continueToConfirmation}
-        >
-          Continuar com a entrega
-        </Button>
-        <Button
-          type="button"
-          title="Clique aqui para retornar a edição de endereço"
-          onClick={returnToDelivery}
-        >
-          Voltar para a edição de endereço
-        </Button>
-      </Sidebar>
-    </CardContainer>
+    <form onSubmit={form.handleSubmit}>
+      <CardContainer>
+        <Overlay onClick={returnToDelivery} />
+        <Sidebar>
+          <h3>Pagamento - Valor a pagar {formataPreco(getTotalPrice())}</h3>
+          <div>
+            <label htmlFor="cardDisplayName">Nome no cartão</label>
+            <input
+              id="cardDisplayName"
+              type="text"
+              value={form.values.cardDisplayName}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {getErrorMessage('cardDisplayName', form.errors.cardDisplayName)}
+            </small>
+          </div>
+          <div>
+            <label htmlFor="cardNumber">Número do cartão</label>
+            <input
+              id="cardNumber"
+              type="text"
+              value={form.values.cardNumber}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {getErrorMessage('cardNumber', form.errors.cardNumber)}
+            </small>
+          </div>
+          <div>
+            <label htmlFor="cardCode">CVV</label>
+            <input
+              id="cardCode"
+              type="text"
+              value={form.values.cardCode}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('cardCode', form.errors.cardCode)}</small>
+          </div>
+          <div>
+            <label htmlFor="cep">CEP</label>
+            <input
+              id="cep"
+              type="text"
+              value={form.values.cep}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>{getErrorMessage('cep', form.errors.cep)}</small>
+          </div>
+          <div>
+            <label htmlFor="expiresMonth">Mês de vencimento</label>
+            <input
+              id="expiresMonth"
+              type="text"
+              value={form.values.expiresMonth}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {getErrorMessage('expiresMonth', form.errors.expiresMonth)}
+            </small>
+          </div>
+          <div>
+            <label htmlFor="expiresYear">Ano de vencimento</label>
+            <input
+              id="expiresYear"
+              type="text"
+              className="margin-bottom"
+              value={form.values.expiresYear}
+              onChange={form.handleChange}
+              onBlur={form.handleBlur}
+            />
+            <small>
+              {getErrorMessage('expiresYear', form.errors.expiresYear)}
+            </small>
+          </div>
+          <Button
+            type="button"
+            title="Clique aqui para continuar com a entrega"
+            onClick={form.handleSubmit}
+          >
+            Continuar com a entrega
+          </Button>
+          <Button
+            type="button"
+            title="Clique aqui para retornar a edição de endereço"
+            onClick={returnToDelivery}
+          >
+            Voltar para a edição de endereço
+          </Button>
+        </Sidebar>
+      </CardContainer>
+    </form>
   );
 };
 
